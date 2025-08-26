463025be0cff16fb113e80057c365a6f26ae3677
Olya Vidzi <olyaVidzi97@gmail.com>
2025-08-26 14:16:43 +0300

    first commit

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,41 @@
 describe('', () => {
+  let user;
+  const article = {
+    title: 'title',
+    description: 'description',
+    body: 'body',
+    tags: ['tag1', 'tag2']
+  };
+
   before(() => {
+    cy.visit('/');
 
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
+
+      cy.register(user.email, user.username, user.password);
+    });
   });
 
-  it('', () => {
+  it('should create and delete article', () => {
+    cy.visit('/');
 
-  });
+    cy.contains('a', 'New Article').click();
 
-  it('', () => {
+    cy.get('[placeholder="Article Title"]').type(article.title);
+    cy.get(`[placeholder="What's this article about?"]`).type(article.description);
+    cy.get('[placeholder="Write your article (in markdown)"]')
+      .type(article.body);
+    article.tags.forEach((tag) => {
+      cy.get('[placeholder="Enter tags"]').type(`${tag}{enter}`);
+    });
 
+    cy.contains('button', 'Publish Article').click();
+
+    cy.contains('.author', user.username.toLowerCase()).should('be.visible');
+
+    cy.contains('button', ' Delete Article').should('be.visible').click();
+
+    cy.url().should('include', '/');
   });
 });
